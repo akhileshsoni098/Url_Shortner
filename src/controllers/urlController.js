3f51398ad0e1896f6e6e5b0f3509cccbb9c1753d
NikithaMerampally <merampallynikitha@gmail.com>
2023-01-17 12:23:09 +0530

    completed phase1

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -4,28 +4,58 @@ const baseUrl="http://localhost:3000";
 const shortid=require("shortid")
 
 const createurl=async function(req,res){
+    try{
+    //--------------if no data is provided in a body-----------------------------------------
     let data=req.body;
-    let longUrl=data.longUrl
-    let validUrl=validator.isURL(longUrl);
-    if(!validUrl){
-        res.status(400).send({msg:"Not a Valid Url"});
-    }
-    let urlCode =shortid.generate(longUrl);
-    let shortUrl=baseUrl+"/"+urlCode;
-    
-   //const data ={longUrl:longUrl,shortUrl:shortUrl,urlCode:urlCode};
-   const url = { longUrl: longUrl, urlCode: urlCode, shortUrl: shortUrl };
-        const Data = await urlModel.create(url)
-        res.status(201).send({ status: true, data: { longUrl: Data.longUrl, shortUrl: Data.shortUrl, urlCode: Data.urlCode } });
-   
-}
+    if(Object.keys(data).length==0){return res.status(400).send({status:false , message:"provide some input of longUrl"})}
+    //------------checking whether the key name is longUrl or not--------------------
 
- const geturl = async function(req,res){
-    let codeUrl=req.params.codeUrl
-    let longUrl=await urlModel.findOne({urlCode:codeUrl}).select({longUrl:1,_id:0})
-    res.status(302).redirect(longUrl.longUrl)
+    let longUrl=data.longUrl
+    if(!longUrl) return res.status(400).send({msg:"please provide key longUrl"})
+    //-----------checking whether type of longUrl is string or not--------------
+    if(typeof(longUrl) != "string"){return res.status(400).send({status:false , message:"Please provide url in a string format "})}
+
+    //------------checking whetther longUrl is valid or not---------------------
+    let validUrl=validator.isURL(longUrl.trim());
+    if(!validUrl){
+        res.status(400).send({status:false,message:"Not a Valid Url"});
+    }
+
+    //-------------checking whether the short url is already generated with this long url-------------------
+    const sameUrl = await urlModel.findOne({longUrl:longUrl}).select({_id:0 , __v:0 ,createdAt:0 ,updatedAt:0})
+        if(sameUrl){return res.status(200).send({status:true , data:sameUrl})}
     
-         
+    //---------if short url is not generated yet with this long url then we are generating short url----------
+    let urlCode =shortid.generate(longUrl)
+    let shortUrl=baseUrl+"/"+urlCode.toLowerCase();
+    
+   const url = { longUrl: longUrl, urlCode: urlCode.toLowerCase(), shortUrl: shortUrl.toLowerCase() };
+        const Data = await urlModel.create(url)
+        res.status(201).send({ status: true, data: { longUrl: Data.longUrl, shortUrl: Data.shortUrl, urlCode: Data.urlCode.toLowerCase() } });
+}catch(error){
+    res.status(500).send({msg:error.message})
+
+} 
+}
+ const geturl = async function(req,res){
+    try{
+    let codeUrl=req.params.codeUrl
+    //------checking whether codeUrl in  path params is provide or not------------ 
+    if(!codeUrl) return res.status(400).send({status:false,message:"please Enter a codeUrl in params"});
+
+    //---------checking whether codeUrl is valid or not-------------
+    let validCodeUrl=shortid.isValid(codeUrl)
+    if(!validCodeUrl) return res.status(400).send({status:false,message:"please Enter a valid CodeUrl"})
+
+   //--------if the codeUrl is valid then we are fetching the data---------- 
+    let longUrl=await urlModel.findOne({urlCode:codeUrl}).select({longUrl:1,_id:0})
+    if(!longUrl) return res.status(404).send({status:false,message:"Url not found"})
+    res.status(302).redirect(longUrl["longUrl"])
+    
+    }catch(error){
+        res.status(500).send({status:false,msg:error.message})
+
+    }      
  }
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,11 @@ const controller=require("../controllers/urlController");
 router.post("/createUrl",controller.createurl);
 router.get("/getUrl/:codeUrl",controller.geturl)
 
+router.all('/*',function (res ,res){
+    res.status(400).send({status :false ,data:"Please send correct url "});
+})
+
+
 module.exports=router;
 
 
